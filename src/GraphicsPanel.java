9e3ff2b715f61190944ca879b9f3128a5876bdf0
BT_1N3_17 <sample@bths.edu>
2024-05-21 08:39:07 -0400

    Something i think

--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -1,2 +1,145 @@
-public class GraphicsPanel {
+import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class GraphicsPanel // extends JPanel implements KeyListener, MouseListener, ActionListener
+        {
+    private BufferedImage background;
+    private Snake Snake;
+    private boolean[] pressedKeys;
+    //private ArrayList<Coin> coins;
+    private Timer timer;
+    private int time;
+
+   /* public GraphicsPanel(String name) {
+        try {
+            background = ImageIO.read(new File("src/assets/background.png"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        player = new Player("src/assets/marioleft.png", "src/assets/marioright.png", name);
+        coins = new ArrayList<>();
+        pressedKeys = new boolean[128];
+        time = 0;
+        timer = new Timer(1000, this); // this Timer will call the actionPerformed interface method every 1000ms = 1 second
+        timer.start();
+        addKeyListener(this);
+        addMouseListener(this);
+        setFocusable(true); // this line of code + one below makes this panel active for keylistener events
+        requestFocusInWindow(); // see comment above
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);  // just do this
+        g.drawImage(background, 0, 0, null);  // the order that things get "painted" matter; we put background down first
+        g.drawImage(player.getPlayerImage(), player.getxCoord(), player.getyCoord(), null);
+
+        // this loop does two things:  it draws each Coin that gets placed with mouse clicks,
+        // and it also checks if the player has "intersected" (collided with) the Coin, and if so,
+        // the score goes up and the Coin is removed from the arraylist
+        for (int i = 0; i < coins.size(); i++) {
+            Coin coin = coins.get(i);
+            g.drawImage(coin.getImage(), coin.getxCoord(), coin.getyCoord(), null); // draw Coin
+            if (player.playerRect().intersects(coin.coinRect())) { // check for collision
+                playCoinSound();
+                player.collectCoin();
+                coins.remove(i);
+                i--;
+            }
+        }
+
+        // draw score
+        g.setFont(new Font("Courier New", Font.BOLD, 24));
+        g.drawString(player.getName() + "'s Score: " + player.getScore(), 20, 40);
+        g.drawString("Time: " + time, 20, 70);
+
+        // player moves left (A)
+        if (pressedKeys[65]) {
+            player.faceLeft();
+            player.moveLeft();
+        }
+
+        // player moves right (D)
+        if (pressedKeys[68]) {
+            player.faceRight();
+            player.moveRight();
+        }
+
+        // player moves up (W)
+        if (pressedKeys[87]) {
+            player.moveUp();
+        }
+
+        // player moves down (S)
+        if (pressedKeys[83]) {
+            player.moveDown();
+        }
+    }
+
+    private void playCoinSound() {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("src/assets/coin.wav").getAbsoluteFile());
+            Clip coinClip = AudioSystem.getClip();
+            coinClip.open(audioInputStream);
+            coinClip.start();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // ----- KeyListener interface methods -----
+    public void keyTyped(KeyEvent e) { } // unimplemented
+
+    public void keyPressed(KeyEvent e) {
+        // see this for all keycodes: https://stackoverflow.com/questions/15313469/java-keyboard-keycodes-list
+        // A = 65, D = 68, S = 83, W = 87, left = 37, up = 38, right = 39, down = 40, space = 32, enter = 10
+        int key = e.getKeyCode();
+        pressedKeys[key] = true;
+    }
+
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+        pressedKeys[key] = false;
+    }
+
+    // ----- MouseListener interface methods -----
+    public void mouseClicked(MouseEvent e) { }  // unimplemented; if you move your mouse while clicking,
+    // this method isn't called, so mouseReleased is best
+
+    public void mousePressed(MouseEvent e) { } // unimplemented
+
+    public void mouseReleased(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1) {  // left mouse click
+            Point mouseClickLocation = e.getPoint();
+            Coin coin = new Coin(mouseClickLocation.x, mouseClickLocation.y);
+            coins.add(coin);
+        } else {
+            Point mouseClickLocation = e.getPoint();
+            if (player.playerRect().contains(mouseClickLocation)) {
+                player.turn();
+            }
+        }
+    }
+
+    public void mouseEntered(MouseEvent e) { } // unimplemented
+
+    public void mouseExited(MouseEvent e) { } // unimplemented
+
+    // ACTIONLISTENER INTERFACE METHODS: used for buttons AND timers!
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() instanceof Timer) {
+            time++;
+        }
+    }
+
+    */
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,2 +1,32 @@
-public class MainFrame {
+import javax.swing.*;
+
+public class MainFrame //implements Runnable
+  {
+
+    private GraphicsPanel panel;
+
+     /* public MainFrame(String name) {
+        JFrame frame = new JFrame("Snake Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(960, 580); // 540 height of image + 40 for window menu bar
+        frame.setLocationRelativeTo(null); // auto-centers frame in screen
+
+        // create and add panel
+        panel = new GraphicsPanel(name);
+        frame.add(panel);
+
+        // display the frame
+        frame.setVisible(true);
+
+        // start thread, required for animation
+        Thread thread = new Thread(this);
+        thread.start();
+    }
+
+    public void run() {
+        while (true) {
+            panel.repaint();  // we don't ever call "paintComponent" directly, but call this to refresh the panel
+        }
+    }
+      */
 }
