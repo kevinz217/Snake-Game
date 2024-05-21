970d3bf1283c7546fd507e8d46b90e59192a0304
BT_1N3_17 <sample@bths.edu>
2024-05-21 09:03:01 -0400

    temporary snake class

--- src/Snake.java
+++ src/Snake.java
@@ -1,2 +1,76 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Snake {
+    private final double MOVE_AMT = 0.4;
+    private String direction;
+    private int score;
+    private BufferedImage right;
+    private BufferedImage left;
+    private double xCoord;
+    private double yCoord;
+    private BufferedImage up;
+    private BufferedImage down;
+    public Snake(String leftImg, String rightImg, String name) {
+        direction = "right";
+        xCoord = 50; // starting position is (50, 435), right on top of ground
+        yCoord = 435;
+        score = 0;
+        try {
+            left = ImageIO.read(new File(leftImg));
+            right = ImageIO.read(new File(rightImg));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public int getxCoord() {
+        return (int) xCoord;
+    }
+
+    public int getyCoord() {
+        return (int) yCoord;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void faceRight() {
+        direction = "right";
+    }
+
+    public void faceLeft() {
+        direction = "left";
+    }
+    public void faceUp() {
+        direction = "up";
+    }
+
+    public void faceDown() {
+        direction = "down";
+    }
+
+    public void move() {
+        if (direction.equals("up")) {
+            yCoord += MOVE_AMT;
+        }
+        if (direction.equals("down")) {
+            yCoord -= MOVE_AMT;
+        }
+        if (direction.equals("left")) {
+            xCoord -= MOVE_AMT;
+        }
+        if (direction.equals("right")) {
+            xCoord += MOVE_AMT;
+        }
+    }
+    public void eatFruit() {
+        score++;
+        // add code for length
+    }
+
 }
